3dd69a8bffe66b044b96fe66a72161397fb03a88
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2024-08-29 13:32:50 +0200

    Update styling to match VNV and TBOT

--- javascript.js
+++ javascript.js
@@ -1,54 +1,30 @@
 window.addEventListener('resize', sizeChanged);
-window.addEventListener('DOMContentLoaded', updateProgressBarAndFadeIn);
-window.onscroll = function () { updateProgressBarAndFadeIn() };
+document.addEventListener('DOMContentLoaded', function () {
+    updateProgressBarAndFadeIn();
+    createImageHandlers();
+    markActiveSection();
+});
+window.onscroll = updateProgressBarAndFadeIn;
 
 function sizeChanged() {
-    if (document.documentElement.clientWidth > 760) {
-        document.getElementById("header").style.marginLeft = "";
-        document.getElementById("sidebar").style.width = "";
-        document.getElementById("flippyHeader").innerText = "🙃"
+    var sidebar = document.getElementsByClassName("left-sidebar");
+    if (sidebar && sidebar.length > 0) {
+        if (document.documentElement.clientWidth > 760) {
+            document.getElementsByClassName("left-sidebar")[0].style.width = "";
+        }
     }
 }
 
 function toggleNav() {
-    if (document.getElementById("sidebar").style.width == 0) {
-        document.getElementById("header").style.marginLeft = "80%";
-        document.getElementById("sidebar").style.width = "80%";
-        document.getElementById("flippyHeader").innerText = "🙂";
+    var sidbear = document.getElementsByClassName("sidebar left-sidebar")[0];
+    if (sidbear.style.width == 0) {
+        sidbear.style.width = "75%";
+        globalThis.menuIsOpen = true;
     }
     else {
-        document.getElementById("header").style.marginLeft = "";
-        document.getElementById("sidebar").style.width = "";
-        document.getElementById("flippyHeader").innerText = "🙃"
+        sidbear.style.width = "";
+        globalThis.menuIsOpen = false;
     }
-}
-
-function refreshRateCalculations() {
-    const rr = document.getElementById("refreshRateInput").value;
-    const confirmation = document.getElementById("applyConfirmation");
-    confirmation.style.opacity = "100%";
-    if (rr > 24) {
-        var rrVRR = Math.round(rr * (1 - rr * 0.00028));
-        const fpsFixed = document.getElementsByClassName("fpsFixed");
-        const fpsVSync = document.getElementsByClassName("fpsVSync");
-        const fpsVRR = document.getElementsByClassName("fpsVRR");
-        const iFpsFixed = document.getElementsByClassName("iFpsFixed");
-        const iFpsVSync = document.getElementsByClassName("iFpsVSync");
-
-        fpsFixed[0].innerHTML = rr;
-        fpsVSync[0].innerHTML = rr - 0.05;
-
-        for (const fpsVRRItem of fpsVRR) {
-            fpsVRRItem.innerHTML = rrVRR;
-        }
-        for (const iFpsFixedItem of iFpsFixed) {
-            iFpsFixedItem.innerHTML = rr;
-        }
-        for (const iFpsVSyncItem of iFpsVSync) {
-            iFpsVSyncItem.innerHTML = rr - 1;
-        }
-    }
-    setTimeout(fadeOut, 2500, confirmation);
 }
 
 function fadeOut(element) {
@@ -80,16 +56,106 @@ function updateProgressBarAndFadeIn() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = window.innerHeight;
 
-    for (var i = 0; i < sections.length; i++) {
-        var sectionTop = sections[i].getBoundingClientRect().top;
-        var sectionHeight = sections[i].clientHeight;
+    if (sections) {
+        for (var i = 0; i < sections.length; i++) {
+            var sectionTop = sections[i].getBoundingClientRect().top;
+            var sectionHeight = sections[i].clientHeight;
 
-        if (sectionTop < height && sectionTop + sectionHeight > 0) {
-            sections[i].classList.add("fade-in");
+            if (sectionTop < height && sectionTop + sectionHeight > 0) {
+                sections[i].classList.add("fade-in");
+            }
         }
     }
 
+    var progressBar = document.getElementsByClassName("progress-bar")[0];
+    if (progressBar) {
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scroll = (winScroll / height);
+        var bottomMargin = (height - 25) / height;
+        progressBar.style.width = scroll * 100 + "%";
+    }
 
-    height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    document.getElementById("myBar").style.width = (winScroll / height) * 100 + "%";
+    var sidebars = document.getElementsByClassName("sidebar");
+    if (sidebars) {
+        var styleVal = "calc(100vh - 6.25em)";
+
+        if (document.documentElement.clientHeight > 900 && scroll > bottomMargin) {
+            styleVal = "calc(100vh - 8.5em)";
+        }
+
+        for (var i = 0; i < sidebars.length; i++) {
+            sidebars[i].style.height = styleVal;
+        }
+    }
+}
+
+function createImageHandlers() {
+
+    const overlay = document.getElementById('image-overlay');
+    const enlargedImage = document.getElementById('enlarged-image');
+
+    if (!enlargedImage || !overlay)
+        return;
+
+    const images = document.querySelectorAll('.content-img');
+    if (!images)
+        return;
+
+    images.forEach(image => {
+        image.addEventListener('click', function () {
+            overlay.style.display = "flex";
+            enlargedImage.src = this.src;
+        });
+    });
+
+    overlay.addEventListener('click', function () {
+        overlay.style.display = "none";
+        enlargedImage.src = '';
+    });
+}
+
+function refreshRateCalculations() {
+    const rr = document.getElementById("refreshRateInput").value;
+    const confirmation = document.getElementById("applyConfirmation");
+    confirmation.style.opacity = "100%";
+    if (rr > 24) {
+        var rrVRR = Math.round(rr * (1 - rr * 0.00028));
+        const fpsFixed = document.getElementsByClassName("fpsFixed");
+        const fpsVSync = document.getElementsByClassName("fpsVSync");
+        const fpsVRR = document.getElementsByClassName("fpsVRR");
+        const iFpsFixed = document.getElementsByClassName("iFpsFixed");
+        const iFpsVSync = document.getElementsByClassName("iFpsVSync");
+
+        fpsFixed[0].innerHTML = rr;
+        fpsVSync[0].innerHTML = rr - 0.05;
+
+        for (const fpsVRRItem of fpsVRR) {
+            fpsVRRItem.innerHTML = rrVRR;
+        }
+        for (const iFpsFixedItem of iFpsFixed) {
+            iFpsFixedItem.innerHTML = rr;
+        }
+        for (const iFpsVSyncItem of iFpsVSync) {
+            iFpsVSyncItem.innerHTML = rr - 1;
+        }
+    }
+    setTimeout(fadeOut, 2500, confirmation);
+}
+
+function markActiveSection() {
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('.sidebar .pageLinks a');
+
+    function setActiveSection() {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 400 < sections[index].offsetTop) { }
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+
+        navLinks[index].classList.add('active');
+    }
+
+    window.addEventListener('scroll', setActiveSection);
+    setActiveSection();
 }
